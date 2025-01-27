99e220480c25793d713c197dc4377a45becec808
Harsh Patel <patelharsh9797@gmail.com>
2025-01-27 06:39:16 +0000

    auth form updated with useForm

--- src/components/auth/signin-form.tsx
+++ src/components/auth/signin-form.tsx
@@ -1,4 +1,7 @@
 "use client";
+import { zodResolver } from "@hookform/resolvers/zod";
+import Link from "next/link";
+import { useForm } from "react-hook-form";
 import LoadingButton from "~/components/loading-button";
 import { Button } from "~/components/ui/button";
 import {
@@ -8,14 +11,34 @@ import {
   CardHeader,
   CardTitle,
 } from "~/components/ui/card";
+import {
+  Form,
+  FormControl,
+  FormField,
+  FormItem,
+  FormLabel,
+  FormMessage,
+} from "~/components/ui/form";
 import { Input } from "~/components/ui/input";
-import { Label } from "~/components/ui/label";
 import { cn } from "~/lib/utils";
+import { signInSchema, type SignInSchemaType } from "~/lib/zod";
 
 export function SigninForm({
   className,
   ...props
 }: React.ComponentPropsWithoutRef<"div">) {
+  const form = useForm<SignInSchemaType>({
+    resolver: zodResolver(signInSchema),
+    defaultValues: {
+      email: "",
+      password: "",
+    },
+  });
+
+  const onSubmit = async (values: SignInSchemaType) => {
+    console.log(values);
+  };
+
   return (
     <div className={cn("flex flex-col gap-6", className)} {...props}>
       <Card>
@@ -26,49 +49,45 @@ export function SigninForm({
           </CardDescription>
         </CardHeader>
         <CardContent>
-          <form>
-            <div className="flex flex-col gap-6">
-              <div className="grid gap-2">
-                <Label htmlFor="email">Email</Label>
-                <Input
-                  id="email"
-                  type="email"
-                  placeholder="m@example.com"
-                  required
+          <Form {...form}>
+            <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-6">
+              {["email", "password"].map((field) => (
+                <FormField
+                  control={form.control}
+                  key={field}
+                  name={field as keyof SignInSchemaType}
+                  render={({ field: fieldProps }) => (
+                    <FormItem>
+                      <FormLabel>
+                        {field.charAt(0).toUpperCase() + field.slice(1)}
+                      </FormLabel>
+                      <FormControl>
+                        <Input
+                          type={field === "password" ? "password" : "email"}
+                          placeholder={`Enter your ${field}`}
+                          {...fieldProps}
+                          autoComplete={
+                            field === "password" ? "current-password" : "email"
+                          }
+                        />
+                      </FormControl>
+                      <FormMessage />
+                    </FormItem>
+                  )}
                 />
-              </div>
-              <div className="grid gap-2">
-                <div className="flex items-center">
-                  <Label htmlFor="password">Password</Label>
-                  <a
-                    href="#"
-                    className="ml-auto inline-block text-sm underline-offset-4 hover:underline"
-                  >
-                    Forgot your password?
-                  </a>
-                </div>
-                <Input id="password" type="password" required />
-              </div>
-              <LoadingButton
-                pending={false}
-                onClick={() => {
-                  console.log("Client");
-                }}
-                className=""
-              >
-                Sign in
-              </LoadingButton>
-              <Button variant="outline" className="w-full">
+              ))}
+              <LoadingButton pending={false}>Sign in</LoadingButton>
+              <Button type="button" variant="outline" className="w-full">
                 Login with Google
               </Button>
-            </div>
-            <div className="mt-4 text-center text-sm">
-              Don&apos;t have an account?{" "}
-              <a href="/signup" className="underline underline-offset-4">
-                Sign up
-              </a>
-            </div>
-          </form>
+              <div className="mt-4 text-center text-sm">
+                Don&apos;t have an account?{" "}
+                <Link href="/signup" className="underline underline-offset-4">
+                  Sign up
+                </Link>
+              </div>
+            </form>
+          </Form>
         </CardContent>
       </Card>
     </div>
--- src/components/auth/signup-form.tsx
+++ src/components/auth/signup-form.tsx
@@ -1,4 +1,7 @@
 "use client";
+import { zodResolver } from "@hookform/resolvers/zod";
+import Link from "next/link";
+import { useForm } from "react-hook-form";
 import LoadingButton from "~/components/loading-button";
 import { Button } from "~/components/ui/button";
 import {
@@ -8,14 +11,36 @@ import {
   CardHeader,
   CardTitle,
 } from "~/components/ui/card";
+import {
+  Form,
+  FormControl,
+  FormField,
+  FormItem,
+  FormLabel,
+  FormMessage,
+} from "~/components/ui/form";
 import { Input } from "~/components/ui/input";
-import { Label } from "~/components/ui/label";
 import { cn } from "~/lib/utils";
+import { signUpSchema, type SignUpSchemaType } from "~/lib/zod";
 
 export function SignupForm({
   className,
   ...props
 }: React.ComponentPropsWithoutRef<"div">) {
+  const form = useForm<SignUpSchemaType>({
+    resolver: zodResolver(signUpSchema),
+    defaultValues: {
+      name: "",
+      email: "",
+      password: "",
+      confirmPassword: "",
+    },
+  });
+
+  const onSubmit = async (values: SignUpSchemaType) => {
+    console.log(values);
+  };
+
   return (
     <div className={cn("flex flex-col gap-6", className)} {...props}>
       <Card>
@@ -26,49 +51,49 @@ export function SignupForm({
           </CardDescription>
         </CardHeader>
         <CardContent>
-          <form>
-            <div className="flex flex-col gap-6">
-              <div className="grid gap-2">
-                <Label htmlFor="email">Email</Label>
-                <Input
-                  id="email"
-                  type="email"
-                  placeholder="m@example.com"
-                  required
+          <Form {...form}>
+            <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-6">
+              {["email", "password"].map((field) => (
+                <FormField
+                  control={form.control}
+                  key={field}
+                  name={field as keyof SignUpSchemaType}
+                  render={({ field: fieldProps }) => (
+                    <FormItem>
+                      <FormLabel>
+                        {field.charAt(0).toUpperCase() + field.slice(1)}
+                      </FormLabel>
+                      <FormControl>
+                        <Input
+                          type={
+                            field.includes("password")
+                              ? "password"
+                              : field === "email"
+                                ? "email"
+                                : "text"
+                          }
+                          placeholder={`Enter your ${field}`}
+                          {...fieldProps}
+                          autoComplete="off"
+                        />
+                      </FormControl>
+                      <FormMessage />
+                    </FormItem>
+                  )}
                 />
-              </div>
-              <div className="grid gap-2">
-                <div className="flex items-center">
-                  <Label htmlFor="password">Password</Label>
-                  <a
-                    href="#"
-                    className="ml-auto inline-block text-sm underline-offset-4 hover:underline"
-                  >
-                    Forgot your password?
-                  </a>
-                </div>
-                <Input id="password" type="password" required />
-              </div>
-              <LoadingButton
-                pending={false}
-                onClick={() => {
-                  console.log("Client");
-                }}
-                className=""
-              >
-                Sign up
-              </LoadingButton>
-              <Button variant="outline" className="w-full">
+              ))}
+              <LoadingButton pending={false}>Sign up</LoadingButton>
+              <Button type="button" variant="outline" className="w-full">
                 Login with Google
               </Button>
-            </div>
-            <div className="mt-4 text-center text-sm">
-              Already have an account?{" "}
-              <a href="/signin" className="underline underline-offset-4">
-                Sign in
-              </a>
-            </div>
-          </form>
+              <div className="mt-4 text-center text-sm">
+                Already have an account?{" "}
+                <Link href="/signin" className="underline underline-offset-4">
+                  Sign in
+                </Link>
+              </div>
+            </form>
+          </Form>
         </CardContent>
       </Card>
     </div>
